d04dc8a4e96769bf0377af1f7c2549fb9182b16a
MustafaYesilDigitanol <mustafa.yesil@digitanol.com>
2022-06-25 11:14:21 +0300

    button

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
   console.log("deneme");
   return (
     <View style={styles.container}>
       <Text>hello Word </Text>
-    
+    <Button title="deneem" ></Button>
       <StatusBar style="auto" />
     </View>
   );
