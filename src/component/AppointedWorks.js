53b0b7879e51ca61c53a3fc1d933e8f0f1dbce6e
MustafaYesilDigitanol <mustafa.yesil@digitanol.com>
2022-06-28 15:37:06 +0300

    28.06.2022_15:36

--- src/component/AppointedWorks.js
+++ src/component/AppointedWorks.js
@@ -1,13 +1,106 @@
 import React from "react";
-import { StyleSheet, Alert, Text, View, Image, TextInput ,TouchableOpacity, AppState} from "react-native";
+import { StyleSheet, Alert, Text, View, Image, TextInput ,TouchableOpacity, AppState,Dimensions} from "react-native";
+import {AppointedWorksDummyData} from "../../data/data";
+import { ScrollView } from 'react-native-gesture-handler';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 const AppointedWorks =(props) => {
     const loginID=props.route.params.loginID;
     const password=props.route.params.password;
+    var dummydata=[];
+    for (let i = 0; i < AppointedWorksDummyData.length; i++){
+        dummydata.push(
+          <View key={i} style={styles.container} >
+          <TouchableOpacity 
+            style={{
+                height:130,
+                width:"95%",
+                flexDirection:"row",  
+                backgroundColor: loginID==AppointedWorksDummyData[i].TayinOlan "#4caf50",
+                borderTopRightRadius: 10,
+                borderTopLeftRadius: 10,
+                borderBottomRightRadius:10,
+                borderBottomLeftRadius:10,}} 
+                onPress={()=>{}} >
+            <View style={styles.ViewStyle1} >
+              <Text style={{fontWeight:"bold", fontSize:13, color:"black"}}>
+                BildirimNo:
+              </Text>
+              <Text style={{fontWeight:"bold", fontSize:12, color:"white"}}>
+                {AppointedWorksDummyData[i].BildirimNo}
+              </Text>
+                <Text style={{fontSize:13, fontWeight:"bold" , color:"black"}}>Tayin Olan:</Text>
+                <Text style={{fontSize:12, fontWeight:"bold", color:"white"}}>{AppointedWorksDummyData[i].TayinOlan}</Text>
+            </View>
+            <View style={styles.ViewStyle2}>
+              <View style={{flexDirection:"row"}}>
+                <Text style={{fontSize:12, fontWeight:"bold"}}>Tenik Birim Tanımı: </Text>
+                <Text style={{fontSize:12, fontWeight:"bold",color:"white"}}>{AppointedWorksDummyData[i].TeknikBirimTanimi}</Text>
+              </View>
+              <View style={{flexDirection:"row"}}>
+                <Text style={{fontSize:12, fontWeight:"bold"}}>Arıza Kodu: </Text>
+                <Text style={{fontSize:12, fontWeight:"bold",color:"white"}}>{AppointedWorksDummyData[i].ArizaKodu} </Text>
+              </View>
+              <View style={{flexDirection:"row"}}>
+                <Text style={{fontSize:12, fontWeight:"bold"}}>Ekipman Tanımı: </Text>
+                <Text style={{fontSize:12, fontWeight:"bold",color:"white"}}>{AppointedWorksDummyData[i].EkipmanTanimi} </Text>
+              </View>
+              <View style={{flexDirection:"row"}}>
+                <Text style={{fontSize:12, fontWeight:"bold"}}>Arıza Bildirim Açıklaması: </Text>
+                <Text style={{fontSize:12, fontWeight:"bold",color:"white"}}>{AppointedWorksDummyData[i].ArizaKoduKisaAciklama}</Text>
+              </View>
+              <View style={{flexDirection:"row"}}>
+                <Text style={{fontSize:12, fontWeight:"bold"}}>Bildirim Tarihi: </Text>
+                <Text style={{fontSize:12, fontWeight:"bold",color:"white"}}>{AppointedWorksDummyData[i].ArizaBaslangic}</Text>
+              </View>
+              <View style={{flexDirection:"row"}}>
+                <Text style={{fontSize:12, fontWeight:"bold" , color:"black"}}>Bildiren:</Text>
+                <Text style={{fontSize:12, fontWeight:"bold", color:"white"}}>{AppointedWorksDummyData[i].Bildiren}</Text>
+              </View> 
+            </View>        
+          </TouchableOpacity>
+          </View>
+        );
+      }
     return(
-        <View>
-            <Text>AppointedWorks {loginID}</Text>
-        </View>
+        <ScrollView>
+        <Text>{dummydata}</Text>
+    </ScrollView>
     );
 }
 export default AppointedWorks;
+const styles = StyleSheet.create({
+    container: {
+      width:width,
+      flex: 1,
+      alignItems: "stretch", 
+      justifyContent: "space-evenly",
+      paddingBottom:10,
+      flexDirection: "row"
+    },
+    scrollViewStyle: {     
+      paddingTop:40,
+      width:width,
+      height:height,
+      flexDirection:"column",
+    },
+    ViewStyle1: {
+      width: "30%",
+      height:"100%",
+      flexDirection:"column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    ViewStyle2: {
+      width: "40%",
+      height:"100%",
+      flexDirection:"column",
+      alignItems: "flex-start",
+      justifyContent: "center",
+    },
+    image: {
+    
+    },
+});
