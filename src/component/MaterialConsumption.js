4e24c789530794cf907ddaadab46f44bffd66b12
MustafaYesilDigitanol <108119637+MustafaYesilDigitanol@users.noreply.github.com>
2022-07-06 17:24:55 +0300

    6 temmuz mustafa son

--- src/component/MaterialConsumption.js
+++ src/component/MaterialConsumption.js
@@ -1,16 +1,37 @@
 import { Text, View } from 'react-native';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import NumericInput from 'react-native-numeric-input';
 
 const MaterialConsumption = (props) =>{
     const loginID=props.route.params.loginID;
     const password=props.route.params.password;
     const SAPKullanici=props.route.params.SAPKullanici;
     const BildirimNo=props.route.params.bildirimNo;
+    const [state,setState]=useState("");
+    const [value,setValue]=useState("");
     return(
         <View>
             <Text>Material Consumption Test Login ID:{loginID}, Password:{password}, SAPKullanici: {SAPKullanici}, BildirimNo: {BildirimNo}</Text>
+            <View>
+                <NumericInput 
+                    value={value} 
+                    onChange={value => setState({value})} 
+                    onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+                    totalWidth={250} 
+                    totalHeight={20} 
+                    iconSize={25}
+                    step={1}
+                    minValue={0}
+                    valueType="integer"
+                    rounded 
+                    textColor='#B0228C' 
+                    iconStyle={{ color: 'white' }} 
+                    rightButtonBackgroundColor='#EA3788' 
+                    leftButtonBackgroundColor='#E56B70' />
+
+            </View>
+            
         </View>
     );
 }
-
 export default MaterialConsumption
